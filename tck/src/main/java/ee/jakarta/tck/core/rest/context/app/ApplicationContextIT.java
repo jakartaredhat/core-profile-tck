7fc0ed402ec70de8063621ea48ce20e4f19c769d
Scott M Stark <starksm64@gmail.com>
2022-04-22 18:38:30 -0500

    Initial WIP
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

---
+++ tck/src/main/java/ee/jakarta/tck/core/rest/context/app/ApplicationContextIT.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) "2022" Red Hat and others
+ *
+ * This program and the accompanying materials are made available under the
+ * Apache Software License 2.0 which is available at:
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package ee.jakarta.tck.core.rest.context.app;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+
+import ee.jakarta.tck.core.rest.JaxRsActivator;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(ArquillianExtension.class)
+public class ApplicationContextIT {
+    @ArquillianResource
+    private URL contextPath;
+
+    @Deployment(testable = false)
+    public static WebArchive createTestArchive() {
+        WebArchive archive = ShrinkWrap.create(WebArchive.class, "rest_context_app.war");
+
+        archive.addClass(SimpleApplicationBean.class)
+                .addClass(ApplicationResource.class)
+                .addClass(JaxRsActivator.class);
+        System.out.printf("rest_context_app: %s\n", archive.toString(true));
+        archive.as(ZipExporter.class).exportTo(new File("/tmp/" + archive.getName()), true);
+        return archive;
+    }
+
+    @Test
+    public void testApplicationContextSharedBetweenJaxRsRequests() throws Exception {
+        URI appIdURI = contextPath.toURI().resolve("rest/application-id");
+        System.out.printf("appIdURI: %s\nSleeping 60 seconds...", appIdURI.toASCIIString());
+        Thread.sleep(60*1000);
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(appIdURI);
+        String request1 = target.request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN).get(String.class);
+        // Make a second request and make sure the same context is used
+        String request2 = target.request(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN).get(String.class);
+        // should be same random number
+        assertEquals(Double.parseDouble(request1), Double.parseDouble(request2));
+    }
+
+}
